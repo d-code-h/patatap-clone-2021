c4cc7fed66d2617e84602c4d5e849ab28c0168fa
David Yunusa <daveed856@outlook.com>
2023-07-24 17:00:43 +0100

    Prepare for deployment

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // CONTROL SHEET
-const   express         =   require('express'),
-        app             =   express();
+const express = require('express'),
+  app = express();
+
+const PORT = process.env.PORT || 3000;
 
 // Configurations
 app.set('view engine', 'ejs');
@@ -8,12 +10,12 @@ app.use(express.static('public'));
 
 // Routes
 app.get('/', (req, res) => {
-    res.render('index');
+  res.render('index');
 });
 
 // Server
-app.listen(3000, function(){
-    console.log('============================');
-    console.log('Server started at port 3000');
-    console.log('============================');
+app.listen(PORT, function () {
+  console.log('============================');
+  console.log(`Server started at port ${PORT}`);
+  console.log('============================');
 });
